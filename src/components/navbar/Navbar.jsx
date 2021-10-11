b9fdeb6c71663d57f49349b970ade46dd8e717a5
dannyvsm <31244585+dannyvsm@users.noreply.github.com>
2021-10-11 20:49:16 +0100

    Added on-hover menus

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,21 +13,37 @@ const Navbar = () => {
     <div className="navbar">
       <div className="container">
         <div className="left">
-            <Film />
+            <Film classname="logo"/>
             <span></span>
             <span>Home</span>
-            <span>Films</span>
+            <div className="menu">
+                <span>Films</span>
+                <ArrowDrop className="icon" />
+                <div class="films">     
+                    <span>By Decade</span>
+                    <span>By Genre</span>
+                    <span>By Director</span>
+                    <span>By Country</span>
+                    <span>Settings</span>
+                    <span>Hidden Gems</span>
+                    <span>Show me something!</span>
+                </div>   
+            </div>
             <span>Lists</span>
             <span>Reviews</span>
         </div>
         <div className="right">
-            <Search />
-            <span></span>
-            <Notifs />
-            <span></span>
-            <Profile />
-            <span></span>
-            <ArrowDrop />
+            <Search  className="icon"/>
+            <Notifs className="icon"/>
+            <Profile className="icon" />
+            <div className="settings">
+                <ArrowDrop className="icon" />
+                <div className="options">
+                    <span>Profile</span>
+                    <span>Settings</span>
+                    <span>Accessibility</span>
+                </div>
+            </div>
         </div>
       </div>
     </div>
