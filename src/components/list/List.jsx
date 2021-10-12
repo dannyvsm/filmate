cf6c05d0be1167b2a333128923a06c274701c98c
dannyvsm <31244585+dannyvsm@users.noreply.github.com>
2021-10-12 14:09:36 +0100

    onClick functionality to buttons

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,18 +1,45 @@
-import React from 'react'
-import "./list.scss"
+import React from 'react';
+import { useRef, useState } from 'react';
+import "./list.scss";
 import ListItem from '../listItem/ListItem';
 import LeftArrow from '@mui/icons-material/ChevronLeft';
 import RightArrow from '@mui/icons-material/ChevronRight';
 
 
 export default function List() {
+
+    const [slideNumber, setSliderNumber] = useState(0)
+
+    const listRef = useRef()
+
+    const handleClick = (direction) => {
+        let distance = listRef.current.getBoundingClientRect().x - 50;
+        if (direction === "left" && slideNumber > 0) {
+            setSliderNumber(slideNumber - 1);
+            listRef.current.style.transform = `translateX(${250 + distance}px)`;
+        }
+        if (direction === "right" && slideNumber < 5) {
+            setSliderNumber(slideNumber + 1);
+            listRef.current.style.transform = `translateX(${-250 + distance}px)`;
+        }
+        console.log(distance)
+    }
+
     return (
         <div className="list">
             <span className="listTitle">Genre Picks</span>
             <div className="wrapper">
-                <LeftArrow class="sliderbtn left"/>
+                <LeftArrow class="sliderbtn left" onClick={() => handleClick("left")}/>
                     {/* <PlayIcon className="playbtn"/> */}
-                    <div className="container">
+                    <div className="container" ref={listRef}>
+                        <ListItem />
+                        <ListItem />
+                        <ListItem />
+                        <ListItem />
+                        <ListItem />
+                        <ListItem />
+                        <ListItem />
+                        <ListItem />
                         <ListItem />
                         <ListItem />
                         <ListItem />
@@ -22,11 +49,11 @@ export default function List() {
                         <ListItem />
                         <ListItem />
                     </div>
-                <RightArrow class="sliderbtn right" />
+                <RightArrow class="sliderbtn right" onClick={() => handleClick("right")}/>
                 </div>
                 <span className="listTitle">Essential Viewing</span>
             <div className="wrapper">
-                <LeftArrow class="sliderbtn left"/>
+                <LeftArrow class="sliderbtn left" onClick={() => handleClick("left")}/>
                     {/* <PlayIcon className="playbtn"/> */}
                     <div className="container">
                         <ListItem />
@@ -37,8 +64,16 @@ export default function List() {
                         <ListItem />
                         <ListItem />
                         <ListItem />
+                        <ListItem />
+                        <ListItem />
+                        <ListItem />
+                        <ListItem />
+                        <ListItem />
+                        <ListItem />
+                        <ListItem />
+                        <ListItem />
                     </div>
-                <RightArrow class="sliderbtn right" />
+                <RightArrow class="sliderbtn right" onClick={() => handleClick("right")}/>
                 </div>
         </div>
     )
